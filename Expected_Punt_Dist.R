17f16e42e017cc759cceab23c7e7f52a8b882ac9
Justin Applefield <jmapple@umich.edu>
2021-11-06 16:49:05 -0400

    Fix punt filtering

--- Expected_Punt_Dist.R
+++ Expected_Punt_Dist.R
@@ -1,6 +1,9 @@
 library(nflfastR)
 future::plan("multisession")
-pbp <- load_pbp(2019:2020, file_type = "qs")
+pbp <- load_pbp(2010:2020, file_type = "qs")
+pbp <- pbp[!is.na(pbp$play_type), ]
+pbp$play_type <- as.factor(pbp$play_type)
+summary(pbp$play_type)
 
 punts <- pbp[pbp$play_type == "punt",]
 punts$end_spot <- 100 - punts$yardline_100 + punts$kick_distance - punts$return_yards
