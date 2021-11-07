6145035a202c7d90d4be025f0c7367065eab1f34
Natalie Seiler <nkseiler@umich.edu>
2021-11-07 11:15:00 -0500

    Changed more variables to factors, and updated how I want to filter field goals into a new colunmn

--- Probability of Successful Field Goal.R
+++ Probability of Successful Field Goal.R
@@ -2,13 +2,13 @@ library(nflfastR)
 future::plan("multisession")
 pbp <- load_pbp(2010:2020, file_type = "qs")
 
-# Making Field Goal Result into a Factor
+# Data Cleaning
 field_goal_result <- as.factor(field_goal_result)
+weather <- as.factor(weather)
+season_type <- as.factor(season_type)
 
 # Filter data to only analyze plays where the field goal was successful
-field_goal_made <- subset(pbp, field_goal_result == 1)
-nrow(field_goal_result)
-nrow(field_goal_made)
+field_goal_made <- subset(pbp, field_goal_attempt == 1 & field_goal_result == 1)
 
 # Run Logistic Regression Modelon successful field goal attempts
 field_goal_glm1 <- glm(field_goal_made ~ weather + yardline_100 + game_seconds_remaining + 
