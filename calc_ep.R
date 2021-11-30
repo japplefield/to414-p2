b180485af9881aff03507707a63a49de495e3b6b
Justin Applefield <jmapple@umich.edu>
2021-11-30 17:23:27 -0500

    Function for calc ep

--- calc_ep.R
+++ calc_ep.R
@@ -1,10 +1,16 @@
+
+my_func <- function(home_team, posteam, oppteam, yardline_100, seconds, prob){
+
+  
+  
+  
 data <- tibble::tibble(
   "season" = 2020,
-  "home_team" = "DET",
-  "posteam" = "GBP",
+  "home_team" = home_team,
+  "posteam" = posteam,
   "roof" = "outdoors",
-  "half_seconds_remaining" = 590,
-  "yardline_100" = 72,
+  "half_seconds_remaining" = seconds,
+  "yardline_100" = yardline_100,
   "down" = 1,
   "ydstogo" = 10,
   "posteam_timeouts_remaining" = 3,
@@ -12,5 +18,28 @@ data <- tibble::tibble(
 )
 
 
-nflfastR::calculate_expected_points(data) %>%
+success <- nflfastR::calculate_expected_points(data) %>%
   dplyr::select(season, yardline_100, td_prob, ep)
+
+data <- tibble::tibble(
+  "season" = 2020,
+  "home_team" = home_team,
+  "posteam" = oppteam,
+  "roof" = "outdoors",
+  "half_seconds_remaining" = seconds,
+  "yardline_100" = 100 - yardline_100,
+  "down" = 1,
+  "ydstogo" = 10,
+  "posteam_timeouts_remaining" = 3,
+  "defteam_timeouts_remaining" = 3
+)
+
+
+failure <- nflfastR::calculate_expected_points(data) %>%
+  dplyr::select(season, yardline_100, td_prob, ep)
+
+return (prob * success$ep + (1-prob) * failure$ep * -1)
+
+}
+
+my_func("DET", "GBP", "DET", 72, 590, 0.8)
